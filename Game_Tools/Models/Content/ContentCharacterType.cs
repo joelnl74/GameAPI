7b26115a976210f8dfd28f74f3f96d7d0a9c3af2
Joey <joelnl74@gmail.com>
2023-09-04 21:49:46 +0200

    Dark

--- Game_Tools/Models/Content/ContentCharacterType.cs
+++ Game_Tools/Models/Content/ContentCharacterType.cs
@@ -3,23 +3,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Tools.Models.Content
 {
-    public enum Type
-    {
-        Unknown = 0,
-        Normale = 1,
-        Grass = 2,
-        Fire = 3,
-        Water = 4,
-        Electric = 5,
-        Rock = 6,
-        Steel = 7,
-        Ground = 8,
-        Fairy = 9,
-        Dark = 10,
-        Ghost = 11,
-        Fighting = 12,
-    }
-
     public class ContentCharacterType
     {
         [Key]
--- Game_Tools/Models/DatabaseObject/BaseCharacterDTO.cs
+++ Game_Tools/Models/DatabaseObject/BaseCharacterDTO.cs
@@ -2,6 +2,36 @@
 
 namespace Tools.Models.DatabaseObject
 {
+    public enum Type
+    {
+        [Display(Name = "Unknown")]
+        Unknown = 0,
+        [Display(Name = "Normal")]
+        Normale = 1,
+        [Display(Name = "Grass")]
+        Grass = 2,
+        [Display(Name = "Fire")]
+        Fire = 3,
+        [Display(Name = "Water")]
+        Water = 4,
+        [Display(Name = "Electric")]
+        Electric = 5,
+        [Display(Name = "Rock")]
+        Rock = 6,
+        [Display(Name = "Steel")]
+        Steel = 7,
+        [Display(Name = "Ground")]
+        Ground = 8,
+        [Display(Name = "Fairy")]
+        Fairy = 9,
+        [Display(Name = "Dark")]
+        Dark = 10,
+        [Display(Name = "Ghost")]
+        Ghost = 11,
+        [Display(Name = "Fighting")]
+        Fighting = 12,
+    }
+
     public class BaseCharacterDTO
     {
         public int id { get; set; }
